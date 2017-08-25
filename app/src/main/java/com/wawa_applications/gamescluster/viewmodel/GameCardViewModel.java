bf96df62563bc07168c7d5441162bb095b20b8e8
wkrzywiec <wojciechkrzywiec@gmail.com>
2017-08-25 06:43:25 +0200

    MainActivity & Game Card UI

---
+++ app/src/main/java/com/wawa_applications/gamescluster/viewmodel/GameCardViewModel.java
@@ -0,0 +1,8 @@
+package com.wawa_applications.gamescluster.viewmodel;
+
+/**
+ * Created by Wojtek Krzywiec on 25/08/2017.
+ */
+
+public class GameCardViewModel {
+}
---
+++ app/src/main/java/com/wawa_applications/gamescluster/viewmodel/MainViewModel.java
@@ -0,0 +1,8 @@
+package com.wawa_applications.gamescluster.viewmodel;
+
+/**
+ * Created by Wojtek Krzywiec on 23/08/2017.
+ */
+
+public class MainViewModel {
+}
