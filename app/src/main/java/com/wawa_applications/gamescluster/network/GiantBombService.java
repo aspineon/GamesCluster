d14bea8db9c9fcfda59037829272425f49f90adb
Wojtek <wojciechkrzywiec@gmail.com>
2017-09-19 16:37:43 +0200

    Add new query to GiantBombService

--- app/src/main/java/com/wawa_applications/gamescluster/network/GiantBombService.java
+++ app/src/main/java/com/wawa_applications/gamescluster/network/GiantBombService.java
@@ -1,9 +1,11 @@
 package com.wawa_applications.gamescluster.network;
 
 
+import com.wawa_applications.gamescluster.model.details.GameDetailsResultModel;
 import com.wawa_applications.gamescluster.model.search.GiantBombSearchModel;
 import io.reactivex.Observable;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 /**
@@ -14,4 +16,8 @@ public interface GiantBombService {
 
     @GET("api/search/?api_key=[API-KEY]&format=json&resources=game&limit=70")
     Observable<GiantBombSearchModel> searchForGames(@Query("query") String gameName);
+
+
+    @GET("api/game/{gameId}/?api_key=[API-KEY]&format=json")
+    Observable<GameDetailsResultModel> getGameDetails(@Path("gameId") String gameId);
 }
